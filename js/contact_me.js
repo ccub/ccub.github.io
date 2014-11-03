7b58f7908f7391f2298f2ececf9eb6bfef714d00
Andrés Cecilia <a.cecilia.luque@gmail.com>
2014-11-03 18:33:32 +0100

    changed language of some messages

--- js/contact_me.js
+++ js/contact_me.js
@@ -34,7 +34,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>Tu mensaje ha sido enviado :) </strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
@@ -46,7 +46,7 @@ $(function() {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append("<strong>Ups, lo sentimos " + firstName + ", ha habido un error al enviar ti mensaje :(");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
